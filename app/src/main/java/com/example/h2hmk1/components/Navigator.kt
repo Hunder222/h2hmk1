adcb6457b9c87965fc01daf36652153ea38e21a1
Elliot Lyhne <Hunterlyhne@gmail.com>
2026-04-09 16:03:08 +0200

    TopNav Merge fix

--- app/src/main/java/com/example/h2hmk1/components/Navigator.kt
+++ app/src/main/java/com/example/h2hmk1/components/Navigator.kt
@@ -1,13 +1,28 @@
 package com.example.h2hmk1.components
 
 import android.R.attr.name
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBackIosNew
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -16,7 +31,7 @@ import com.example.h2hmk1.components.PremiumHome
 import com.example.h2hmk1.components.TakeABreathHome
 
 @Composable
-fun Navigator(name : String) {
+fun Navigator() {
     val navController = rememberNavController()
 
     TopNav(navController)
